dbe7289b90b4802b6004fa5210efba6141f68d3a
raghu693 <sonia.nagar16@gmail.com>
2025-05-24 08:47:04 +0530

    second commit

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ class Employee:
         difference = a-b
         print(difference)
 
+    def multiply(a, b):
+        product = a*b
+        print(product)
 
 Employee.add( 12, 12)
 Employee.substract( 12, 13)
-Employee.add( 12, 12)
+Employee.multiply( 12, 12)
