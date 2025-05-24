51bbfb3e613266d16f83618d142198e18f61aa98
raghu693 <sonia.nagar16@gmail.com>
2025-05-24 08:45:15 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self):
+        print("Constructor Running, Right Now");
+
+    def add(a, b):
+        sum = a + b
+        print(sum)
+
+    def substract(a, b):
+        difference = a-b
+        print(difference)
+
+
+Employee.add( 12, 12)
+Employee.substract( 12, 13)
+Employee.add( 12, 12)
